42e2cb6ddbfdceb804717de5835c3828f32b7eac
Dmitry_Nazarov <roygbiv1969@yandex.ru>
2026-06-13 21:47:51 +0300

    Initial commit

---
+++ Source/Tapesick/Tapesick.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Tapesick : ModuleRules
+{
+	public Tapesick(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Tapesick",
+			"Tapesick/Variant_Horror",
+			"Tapesick/Variant_Horror/UI",
+			"Tapesick/Variant_Shooter",
+			"Tapesick/Variant_Shooter/AI",
+			"Tapesick/Variant_Shooter/UI",
+			"Tapesick/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
