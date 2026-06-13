53e48d619fae9fe0a7a1b32f394f8a3cb18a899d
Dmitry Nazarov <roygbiv1969@yandex.ru>
2026-06-13 22:11:44 +0300

    Clean (#1)

--- Source/Tapesick/Tapesick.Build.cs
+++ Source/Tapesick/Tapesick.Build.cs
@@ -13,32 +13,13 @@ public class Tapesick : ModuleRules
 			"CoreUObject",
 			"Engine",
 			"InputCore",
-			"EnhancedInput",
-			"AIModule",
-			"StateTreeModule",
-			"GameplayStateTreeModule",
-			"UMG",
-			"Slate"
+			"EnhancedInput"
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		PublicIncludePaths.AddRange(new string[] {
-			"Tapesick",
-			"Tapesick/Variant_Horror",
-			"Tapesick/Variant_Horror/UI",
-			"Tapesick/Variant_Shooter",
-			"Tapesick/Variant_Shooter/AI",
-			"Tapesick/Variant_Shooter/UI",
-			"Tapesick/Variant_Shooter/Weapons"
+			"Tapesick"
 		});
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
