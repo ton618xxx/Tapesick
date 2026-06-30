59774852d1f3fa7a350d99840413685198f3e576
Dmitry_Nazarov <roygbiv1969@yandex.ru>
2026-06-30 14:49:45 +0300

    Upgrade to 5.8

--- Source/TapesickEditor.Target.cs
+++ Source/TapesickEditor.Target.cs
@@ -8,8 +8,8 @@ public class TapesickEditorTarget : TargetRules
 	public TapesickEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
-		ExtraModuleNames.Add("Tapesick");
+        DefaultBuildSettings = BuildSettingsVersion.V7;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
+        ExtraModuleNames.Add("Tapesick");
 	}
 }
